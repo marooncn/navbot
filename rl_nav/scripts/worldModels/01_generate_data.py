bbf0db28cf053ef390188318c7fd1b66fca5fcbe
Liulong Ma <LiulongMa@outlook.com>
2019-01-24 20:51:30 +0800

    Add files via upload

--- rl_nav/scripts/worldModels/01_generate_data.py
+++ rl_nav/scripts/worldModels/01_generate_data.py
@@ -16,46 +16,48 @@ def main(args):
     maze_id = args.maze_id
     GazeboMaze = env.GazeboMaze(maze_id=maze_id, continuous=True)
     print("Generating data for env maze{}".format(maze_id))
-    total_frames = 0
-    obs_data = []
-    action_data = []
     random_generated_int = np.random.randint(0, 2 ** 31 - 1)
     np.random.seed(random_generated_int)
-    episode = 0
-    while total_frames < record_frames:
-        print('---------------------------')
-        obs_sequence = []
-        action_sequence = []
+    for i in range(50):
+        total_frames = 0
+        obs_data = []
+        action_data = []
+        episode = 0
+        while total_frames < record_frames:
+            print('---------------------------')
+            obs_sequence = []
+            action_sequence = []
 
-        observation = GazeboMaze.reset()
-        done = False
-        t = 0
+            observation = GazeboMaze.reset()
+            done = False
+            t = 0
 
-        while not done:
-            action = [np.random.uniform(0, 1), np.random.uniform(-1, 1)]
-            # print(action)
-            obs_sequence.append(observation)
-            action_sequence.append(action)
-            t += 1
+            while not done:
+                action = [np.random.uniform(0, 1), np.random.uniform(-1, 1)]
+                # print(action)
+                obs_sequence.append(observation)
+                action_sequence.append(action)
+                t += 1
 
-            observation, done, reward = GazeboMaze.execute(action)
+                observation, done, reward = GazeboMaze.execute(action)
 
-        total_frames += t
-        episode += 1
-        obs_data.append(obs_sequence)
-        action_data.append(action_sequence)
-        print("Episode {} finished after {} timesteps".format(episode, t + 1))
-        print("Current dataset contains {} observations".format(total_frames))
+            total_frames += t
+            episode += 1
+            obs_data.append(obs_sequence)
+            action_data.append(action_sequence)
+            print("{}th collections".format(i))
+            print("Episode {} finished after {} timesteps".format(episode, t + 1))
+            print("Current dataset contains {} observations".format(total_frames))
 
-    print("Saving dataset ...")
-    np.save(dir_name+'/observation', obs_data)
-    np.save(dir_name+'/action', action_data)
+        print("Saving dataset ...")
+        np.save(dir_name+'/observation'+str(i+4), obs_data)
+        np.save(dir_name+'/action'+str(i+4), action_data)
     GazeboMaze.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate new training data')
-    parser.add_argument('--record_frames', type=int, default=40000, help='how many frames you need to record')
+    parser.add_argument('--record_frames', type=int, default=50000, help='how many frames you need to record')
     parser.add_argument('--maze_id', type=int, default=0, help='which maze ')
     args = parser.parse_args()
     main(args)
