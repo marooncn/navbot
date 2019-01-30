bb7e1c6f4ce9fcbbdf0ffefafd4b5b399c92034c
Liulong Ma <LiulongMa@outlook.com>
2019-01-30 14:40:37 +0800

    Add files via upload

--- rl_nav/scripts/worldModels/03_generate_rnn_data.py
+++ rl_nav/scripts/worldModels/03_generate_rnn_data.py
@@ -1,16 +1,23 @@
 import numpy as np
-import argparse
+import sys
+import os
 import VAE
 sys.path.append("..")
 import config
 
+
+dir_name = 'rnn_data'
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
+
 vae = VAE.VAE()
 vae.set_weights(config.vae_weight)
 
-for i in range(95, 98):
+for i in range(100):
     obs_data = np.load('./record/observation{}'.format(i) + '.npy')
     action_data = np.load('./record/action{}'.format(i) + '.npy')
 
     rnn_input, rnn_output = vae.generate_rnn_data(obs_data, action_data)
-    np.save('./data/rnn_input_' + str(i), rnn_input)
-    np.save('./data/rnn_output_' + str(i), rnn_output)
+    np.save(dir_name+'/rnn_input_' + str(i), rnn_input)
+    np.save(dir_name+'/rnn_output_' + str(i), rnn_output)
+    print("Saving succeeds!")
