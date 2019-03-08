3b0be43b53711b6e3ea3fc4ce54a6b6bf7e37caa
Liulong Ma <LiulongMa@outlook.com>
2019-03-08 16:06:59 +0800

    Add files via upload

--- rl_nav/scripts/worldModels/04_train_rnn.py
+++ rl_nav/scripts/worldModels/04_train_rnn.py
@@ -21,21 +21,21 @@ def main(args):
             print("Either set --new_model or ensure {} exists".format(config.rnn_weight))
             raise
 
-    for i in range(100):
+    for i in range(10, 130):
         print('Building {}th...'.format(i))
-        rnn_input = np.load('./rnn_data/rnn_input_' + str(i) + '.npy', encoding='latin1')
-        rnn_output = np.load('./rnn_data/rnn_output_' + str(i) + '.npy', encoding='latin1')
-        # sequence pre-processing, for training LSTM the rnn_input must be (samples/epochs, time steps, features)
+        rnn_input = np.load('./rnn_data/rnn_input_' + str(i) + '.npy')
+        rnn_output = np.load('./rnn_data/rnn_output_' + str(i) + '.npy')
+        # sequence pre-processing, for training LSTM the rnn_input must be (samples/episodes, time steps, features)
         rnn_input = pad_sequences(rnn_input, maxlen=40, dtype='float32', padding='post', truncating='post')
         rnn_output = pad_sequences(rnn_output, maxlen=40, dtype='float32', padding='post', truncating='post')
-        # print(rnn_input.shape)
-        # print(rnn_output.shape)
+        print(rnn_input.shape)
+        print(rnn_output.shape)
         rnn.train(rnn_input, rnn_output, i)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=('Train RNN'))
-    parser.add_argument('--new_model', type=bool, default=True, help='start a new model from scratch?')
+    parser.add_argument('--new_model', type=bool, default=False, help='start a new model from scratch?')
     args = parser.parse_args()
 
     main(args)
--- rl_nav/scripts/worldModels/RNN.py
+++ rl_nav/scripts/worldModels/RNN.py
@@ -1,5 +1,4 @@
 import math
-import numpy as np
 
 from keras.layers import Input, LSTM, Dense
 from keras.models import Model
@@ -7,6 +6,7 @@ from keras import backend as K
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam, SGD
 import sys
+import numpy as np
 sys.path.append("..")
 import config
 
@@ -16,11 +16,12 @@ ACTION_DIM = 2
 HIDDEN_UNITS = 256
 GAUSSIAN_MIXTURES = 5
 
-BATCH_SIZE = 128
-EPOCHS = 20
+BATCH_SIZE = 64
+EPOCHS = 1
 
 
-def get_mixture_coef(y_pred):
+# separate coefficients from NN output
+def get_mdn_coef(y_pred):
     d = GAUSSIAN_MIXTURES * Z_DIM
 
     rollout_length = K.shape(y_pred)[1]
@@ -40,20 +41,56 @@ def get_mixture_coef(y_pred):
     return pi, mu, sigma  # , discrete
 
 
+# compute the distribution value under y_true
 def tf_normal(y_true, mu, sigma, pi):
     rollout_length = K.shape(y_true)[1]
     y_true = K.tile(y_true, (1, 1, GAUSSIAN_MIXTURES))
     y_true = K.reshape(y_true, [-1, rollout_length, GAUSSIAN_MIXTURES, Z_DIM])
 
-    oneDivSqrtTwoPI = 1 / math.sqrt(2 * math.pi)
     result = y_true - mu
-    #   result = K.permute_dimensions(result, [2,1,0])
-    result = result * (1 / (sigma + 1e-8))
-    result = -K.square(result) / 2
-    result = K.exp(result) * (1 / (sigma + 1e-8)) * oneDivSqrtTwoPI
+    result = result * (1.0 / (sigma + 1e-8))
+    result = -K.square(result) / 2.0
+    result = K.exp(result) * 1.0 / ((math.sqrt(2 * math.pi))*(sigma + 1e-8))
     result = result * pi
-    result = K.sum(result, axis=2)  # sum over gaussians
-    # result = K.prod(result, axis=2) # multiply over latent dims
+    result = K.sum(result, axis=2)  # sum over gaussian
+    # constrain the max value of each element is no more than 1.0
+    # result = K.expand_dims(result, -1)
+    # max_value = K.ones_like(result)
+    # result = K.concatenate([result, max_value], -1)
+    # K.min(result, -1, keepdims=False)
+    return result
+
+
+# samples from a categorial distribution
+def get_pi_idx(x, pdf):
+    accumulate = 0
+    for i in range(GAUSSIAN_MIXTURES):
+        accumulate += K.get_value(pdf[i])
+        # print(accumulate)
+        if accumulate >= x:
+            return i
+    print('error with sampling ensemble')
+    return -1
+
+
+# generates a random sequence using the trained model
+def generate_ensemble(out_pi, out_mu, out_sigma, M = 10):
+    result = np.random.rand(Z_DIM, M)  # initially random [0, 1]
+    rn = np.random.randn(Z_DIM, M)  # normal random matrix (0.0, 1.0)
+    out_pi = K.reshape(out_pi, [GAUSSIAN_MIXTURES, Z_DIM])
+    out_pi = K.permute_dimensions(out_pi, (1, 0))
+    # print(out_pi.shape)
+    out_mu = K.reshape(out_mu, [GAUSSIAN_MIXTURES, Z_DIM])
+    out_sigma = K.reshape(out_sigma, [GAUSSIAN_MIXTURES, Z_DIM])
+
+    # transforms result into random ensembles
+    for j in range(M):
+        for i in range(Z_DIM):
+            idx = get_pi_idx(result[i, j], out_pi[i])
+            mu = out_mu[idx, i]
+            std = out_sigma[idx, i]
+            result[i, j] = K.get_value(mu) + rn[i, j] * K.get_value(std)
+            # print(result[i, j])
     return result
 
 
@@ -87,33 +124,36 @@ class RNN():
 
         # loss function
         def rnn_r_loss(y_true, y_pred):
-            pi, mu, sigma = get_mixture_coef(y_pred)
+            pi, mu, sigma = get_mdn_coef(y_pred)
 
             result = tf_normal(y_true, mu, sigma, pi)
 
-            result = -K.log(result + 1e-8)
-            result = K.mean(result, axis=(1, 2))  # mean over rollout length and z dim
-
+            # result = -K.log(result + 1e-8)
+            # result = K.mean(result, axis=(1, 2))  # mean over rollout length and z dim
+            result = K.max(result, axis=(1, 2))
             return result
 
         def rnn_kl_loss(y_true, y_pred):
-            pi, mu, sigma = get_mixture_coef(y_pred)
+            pi, mu, sigma = get_mdn_coef(y_pred)
             kl_loss = - 0.5 * K.mean(1 + K.log(K.square(sigma)) - K.square(mu) - K.square(sigma), axis=[1, 2, 3])
+            kl_tolerance = 0.5
+            kl_loss = K.maximum(kl_loss, kl_tolerance * Z_DIM)
             return kl_loss
 
         def rnn_loss(y_true, y_pred):
             return rnn_r_loss(y_true, y_pred)  # + rnn_kl_loss(y_true, y_pred)
 
-        optimizer = Adam(lr=0.0001)
+        # optimizer = Adam(lr=0.0001)
+        optimizer = SGD(lr=0.0001, decay=1e-4, momentum=0.9, nesterov=True)
         rnn.compile(loss=rnn_loss, optimizer=optimizer, metrics=[rnn_r_loss, rnn_kl_loss])
 
-        return (rnn, forward)
+        return [rnn, forward]
 
     def set_weights(self, filepath):
         self.model.load_weights(filepath)
 
     def train(self, rnn_input, rnn_output, i=0):
-        earlystop = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=1, mode='auto')
+        earlystop = EarlyStopping(monitor='rnn_r_loss', min_delta=0.01, patience=5, verbose=1, mode='auto')
         callbacks_list = [earlystop]
 
         self.model.fit(rnn_input, rnn_output,
@@ -127,3 +167,10 @@ class RNN():
 
     def save_weights(self, filepath):
         self.model.save_weights(filepath)
+
+    # predict the output
+    def get_output(self, input_data):
+        output = self.model.predict(input_data)
+        pi, mu, sigma = get_mdn_coef(output)
+        result = generate_ensemble(pi, mu, sigma)
+        return result
