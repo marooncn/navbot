83f93295b32069d3b8e8fb4b18c087d2c10c4d74
Liulong Ma <LiulongMa@outlook.com>
2019-03-12 20:51:00 +0800

    Add files via upload

--- rl_nav/scripts/E2E_DQN.py
+++ rl_nav/scripts/E2E_DQN.py
@@ -1,15 +1,25 @@
 from tensorforce.agents import DQNAgent
 import os
 import env
+import config
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-GazeboMaze = env.GazeboMaze(maze_id=0, continuous=False)
-
-dir_name = 'record'
-if not os.path.exists(dir_name):
-    os.makedirs(dir_name)
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+maze_id = config.maze_id
 restore = False
+
+GazeboMaze = env.GazeboMaze(maze_id=maze_id, continuous=False)
+
+record_dir = 'record'
+if not os.path.exists(record_dir):
+    os.makedirs(record_dir)
+
+saver_dir = './models/nav{}'.format(maze_id)
+if not os.path.exists(saver_dir):
+    os.makedirs(saver_dir)
+
+summarizer_dir = './record/E2E_DQN/nav{}'.format(maze_id)
+if not os.path.exists(summarizer_dir):
+    os.makedirs(summarizer_dir)
 
 states = dict(
     image=GazeboMaze.states,
@@ -31,23 +41,12 @@ network_spec = [
           dict(type='flatten'),
           dict(type='output', name='image_output')
      ],
-     [
-          dict(type='input', names=['previous_act']),
-          dict(type='dense', size=32, activation='relu'),
-          dict(type='dense', size=16, activation='relu'),
-          dict(type='output', name='action_output'),
-     ],
-     [
-          dict(type='input', names=['relative_pos']),
-          dict(type='dense', size=32, activation='relu'),
-          dict(type='dense', size=16, activation='relu'),
-          dict(type='output', name='position_output'),
-     ],
+
 
      [
-          dict(type='input', names=['image_output', 'action_output', 'position_output'], aggregation_type='concat'),
-          dict(type='dense', size=64, activation='relu'),
-          dict(type='dense', size=32, activation='relu'),
+          dict(type='input', names=['image_output', 'previous_act', 'relative_pos'], aggregation_type='concat'),
+          dict(type='dense', size=512, activation='relu'),
+          dict(type='dense', size=512, activation='relu'),
 
      ]
 
@@ -60,24 +59,23 @@ memory = dict(
 )
 
 exploration = dict(
-    type='epsilon_decay',
+    type='epsilon_anneal',
     initial_epsilon=1.0,
-    final_epsilon=0.1,
-    timesteps=100000,
-    start_timestep=0
+    final_epsilon=0.2,
+    timesteps=500000
 )
 
 update_model = dict(
     unit='timesteps',
-    # 64 timesteps per update
-    batch_size=64,
-    # Every 64 timesteps
-    frequency=64
+    # 32 timesteps per update
+    batch_size=32,
+    # Every 4 timesteps
+    frequency=4
 )
 
 optimizer = dict(
     type='adam',
-    learning_rate=0.0001
+    learning_rate=5e-3
 )
 
 # Instantiate a Tensorforce agent
@@ -85,18 +83,21 @@ agent = DQNAgent(
     states=states,
     actions=GazeboMaze.actions,
     network=network_spec,
-    update_mode=update_model,
-    memory=memory,
+    # update_mode=update_model,
+    # memory=memory,
     actions_exploration=exploration,
-    saver=dict(directory='./models', basename='E2E_DQN_model.ckpt', load=restore, seconds=600),
-    summarizer=dict(directory='./record/E2E_DQN', labels=["graph", "losses", "reward", "'entropy'"], seconds=600),
+    saver=dict(directory=saver_dir, basename='E2E_DQN_model.ckpt', load=restore, seconds=10800),
+    summarizer=dict(directory=summarizer_dir, labels=["graph", "losses", "reward"], seconds=10800),
     optimizer=optimizer,
-    double_q_model=True
+    # # target_sync_frequency=1000,  # Target network sync frequency
+    double_q_model=True,
+    huber_loss=1.0  # Huber loss clipping
 )
 
 
 episode = 0
 episode_rewards = []
+successes = []
 total_timestep = 0
 max_timesteps = 1000
 max_episodes = 50000
@@ -112,16 +113,14 @@ while True:
     success = False
 
     while True:
-        state = dict()
-        state['image'] = observation,
-        state['previous_act'] = GazeboMaze.vel_cmd,
-        state['relative_pos'] = GazeboMaze.p,
+        previous_act = GazeboMaze.vel_cmd
+        print(previous_act)
+        relative_pos = GazeboMaze.p
+        state = dict(image=observation, previous_act=previous_act, relative_pos=relative_pos)
         # state = dict(image=observation, previous_act=GazeboMaze.vel_cmd, relative_pos=GazeboMaze.p)
-        # print(state)
 
         # Query the agent for its action decision
         action = agent.act(state)
-        print(action)
         # Execute the decision and retrieve the current information
         observation, terminal, reward = GazeboMaze.execute(action)
         observation = observation / 255.0  # normalize
@@ -138,15 +137,24 @@ while True:
     total_timestep += timestep
     # avg_reward = float(episode_reward)/timestep
     episode_rewards.append([episode_reward, timestep, success])
-    if total_timestep > 100000:
-        print('{}th episode reward: {}'.format(episode, episode_reward))
+
+    # if total_timestep > 100000:
+    #     print('{}th episode reward: {}'.format(episode, episode_reward))
 
     if episode % 1000 == 0:
-        f = open(dir_name + '/E2E_DQN_episode' + str(episode) + '.txt', 'w')
+        f = open(record_dir + '/E2E_DQN_episode' + str(episode) + '.txt', 'w')
         for i in episode_rewards:
             f.write(str(i))
             f.write('\n')
         f.close()
+        episode_rewards = []
+        agent.save_model('./models/')
 
-    if episode == max_episodes:
-        break
+    if len(successes) > 100:
+        if sum(successes[-100:]) > 90:
+            GazeboMaze.close()
+            agent.save_model('./models/')
+            break
+
+    # if episode == max_episodes:
+    #     break
