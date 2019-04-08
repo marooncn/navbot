6d156f14dd4d38a41c35b528b74bcbe45c308f45
Liulong Ma <LiulongMa@outlook.com>
2019-04-08 23:08:20 +0800

    Add files via upload

--- tools/draw_success_rate.py
+++ tools/draw_success_rate.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import numpy as np
 import matplotlib.pyplot as plt
-import mpl_toolkits.axisartist as axisartist
 
-# 创建画布
-fig = plt.figure()
-# 使用axisartist.Subplot方法创建一个绘图区对象ax
-ax = axisartist.Subplot(fig, 111)
-# 将绘图区对象添加到画布中
-fig.add_axes(ax)
-# 通过set_axisline_style方法设置绘图区的底部及左侧坐标轴样式
-# "-|>"代表实心箭头："->"代表空心箭头
-ax.axis["bottom"].set_axisline_style("->", size = 1.5)
-ax.axis["left"].set_axisline_style("->", size = 1.5)
-# 通过set_visible方法设置绘图区的顶部及右侧坐标轴隐藏
-ax.axis["top"].set_visible(False)
-ax.axis["right"].set_visible(False)
+plt.rcParams['font.sans-serif'] = ['Times New Roman']  # 如果要显示中文字体,则在此处设为：SimHei
+plt.rcParams['axes.unicode_minus'] = False  # 显示负号
+plt.figure(figsize=(10, 6))
+plt.grid(linestyle="--")  # 设置背景网格线为虚线
+ax = plt.gca()
+ax.spines['top'].set_visible(False)  # 去掉上边框
+ax.spines['right'].set_visible(False)  # 去掉右边框
 
 record_path = '../materials/record/'
 
@@ -25,7 +17,7 @@ record_path = '../materials/record/'
 def main():
     f = open(record_path + 'PPO_nav1.txt', 'r')
     lines = f.readlines()
-    success = []    
+    success = []
     successes = []
 
     for i in range(len(lines)):
@@ -40,12 +32,12 @@ def main():
         success_rate = sum(success)
         successes.append(success_rate)
         success = []
- 
+
     f.close()
 
     f2 = open(record_path + 'E2E_PPO_nav1.txt', 'r')
     lines2 = f2.readlines()
-    success2 = []    
+    success2 = []
     successes2 = []
 
     for i in range(100, len(lines2)):
@@ -63,14 +55,24 @@ def main():
 
     f2.close()
 
-    fig = plt.gcf()
-    plt.xlabel('episode')
-    plt.ylabel('success rate(%)')
-    plt.plot(range(len(successes)), successes)
-    plt.plot(range(len(successes2)), successes2)
-    plt.legend(['PPO', 'E2E_PPO'])
-    plt.title('maze1')
-    fig.savefig('../result/maze1_dense_success.png')
+    plt.plot(range(len(successes)), successes, color="blue", label="Proposed", linewidth=1.5)
+    plt.plot(range(len(successes2)), successes2, color="green", label="Benchmark", linewidth=1.5)
+
+    size = 22
+    plt.xticks(fontsize=size, fontweight='bold')  # 默认字体大小为10
+    plt.yticks(fontsize=size, fontweight='bold')
+    # plt.title("example", fontsize=12, fontweight='bold')  # 默认字体大小为12
+    plt.xlabel("episode", fontsize=size, fontweight='bold')
+    plt.ylabel("success rate(%)", fontsize=size, fontweight='bold')
+
+    plt.title('maze1', fontsize=size, fontweight='bold')
+    # plt.legend()          #显示各曲线的图例
+    plt.legend(loc=4, numpoints=1)  # lower right
+    leg = plt.gca().get_legend()
+    ltext = leg.get_texts()
+    plt.setp(ltext, fontsize=size, fontweight='bold')  # 设置图例字体的大小和粗细
+
+    plt.savefig('../result/maze1_dense_success.png')
     plt.show()
 
 
